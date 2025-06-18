64927f257f61cdee632ef7a31dcd5999673be29a
BrijeshSukhadiya <67324310+BrijeshSukhadiya@users.noreply.github.com>
2025-06-18 14:34:19 +0200

    weight-decay_FusedAdamW

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,3 +1,4 @@
+import inspect
 import math
 from dataclasses import dataclass
 import torch
@@ -181,6 +182,29 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+    
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
 
 # -----------------------------------------------------------------------------
 import tiktoken
@@ -231,6 +255,7 @@ model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 # model = torch.compile(model) ## NOT AVAILABLE ON WINDOWS
 
+
 max_lr = 3e-4
 min_lr = max_lr * 0.1
 warmup_steps = 10
@@ -250,7 +275,8 @@ def get_lr(it):
 
 import time
 # optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas= (0.9, 0.95), eps=1e-8)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas= (0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimixer(weight_decay=0.1, learning_rate=6e-4, device=device)
 for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
